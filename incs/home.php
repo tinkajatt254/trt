5ab6ceb58efbfab18476a2aeb29c09e92fdf27d8
Ali Phularwan <victamoflove@gmail.com>
2020-03-18 14:20:03 +0500

    pousg

--- incs/home.php
+++ incs/home.php
@@ -194,21 +194,17 @@
 
 <div class="container" style="padding: 20px 0px;">
 
-	<div class="">
-		
+	<div class="justify-center">
+		<h3 style="margin: 20px 10px;"> Get in Touch  </h3>
 	</div>
 
 	<div class="row">
-		<div class="col-sm-6">
-		
-		<h3 style="margin: 0px 10px;"> Get in Touch  </h3>
-		<div style="width: 100%;height: 33vh;border: solid 1px #000;margin: 10px;">
-			map here
-		</div>
+		<div class="col-sm-8">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3365.1744132612694!2d74.48444961520785!3d32.49476720563024!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391ee9921a0183a3%3A0x9c1df0ca1551d436!2sLexer%20international!5e0!3m2!1sen!2s!4v1584522981024!5m2!1sen!2s" width="600" height="450" frameborder="0" style="border:0;border-radius: 10px;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 
 	</div>
 
-	<div class="col-sm-6 justify-center">
+	<div class="col-sm-4 justify-center">
 		<form class="px-3 py-4">
 			<div class="form-group">
 				<input type="text" class="form-control" placeholder="your name">
