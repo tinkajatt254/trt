99078d8a243fc3177293d3f5a15a12a5eba4214d
Ali Phularwan <victamoflove@gmail.com>
2020-03-18 17:39:07 +0500

    push

---
+++ incs/home2.php
@@ -0,0 +1,153 @@
+<style type="text/css">
+	a:hover{
+		text-decoration: none;
+	}
+	.slider-div{
+		width: 100%;
+		height: 80vh;
+		background: url("imgs/banner.jpg");
+		background-repeat: no-repeat;
+		background-size: cover;
+	}
+	.inner-slider{
+		width: 100%;
+		height: 80vh;
+		display: flex;
+		background-color: rgba(255,255,255,0.5);
+		flex-direction: column;
+		justify-content: center;
+	}
+	.slider-action-btn{
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+	}
+	.slider-action-btn a{
+		background: #46a7ad;
+		color: #fff;
+		border-radius: 10px;
+		padding: 10px 20px;
+		margin: 10px 20px;
+	}
+	.slider-action-btn a:last-child{
+		border: solid 1px #46a7ad;
+		background-color: transparent;
+		color: #000;
+	}
+	.category-section{
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+	.in-about-section{
+		background: #46a7ad;
+	}
+	.in-abt-inner{
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+</style>
+<div class="slider-div">
+	<div class="inner-slider">
+		<h1 class="text-center my-5"> Greeting! from <br>Tony Rockwell Tools </h1>
+		<center>
+			<p style="width: 65%;line-height: 35px;">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</center>
+
+		<div style="margin-top: 30px;" class="slider-action-btn">
+			<a href="#"> Categories </a>
+			<a href="#"> Read More </a>
+		</div>
+
+	</div>
+</div>
+
+<!-- index about section -->
+
+<div class="in-about-section">
+	<div class="container">
+		<div class="row in-abt-inner">
+			<div class="col-md-8">
+				<h2> About Us </h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</p>
+			</div>
+			<div class="col-md-4">
+				<img src="imgs/dental.jpg">
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="category-section py-5">
+	<div class="container">
+		<h2 class="text-center "> Our Categories </h2>
+		<p class="text-center">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+		</p>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<!-- card 1 -->
+			<div class="col-sm-4">
+				<div class="card">
+				  <div class="card-body">
+				    <h4 class="card-title">Dental Instruments</h4>
+				    <p class="card-text">Some example text. Some example text.</p>
+				    <a href="#" class="card-link btn btn-success">Explore</a>
+				    <a href="#" class="card-link">Featured Products</a>
+				  </div>
+				</div>
+			</div>
+			<!-- card 2 -->
+			<div class="col-sm-4">
+				<div class="card">
+				  <div class="card-body">
+				    <h4 class="card-title">Surgical Instruments</h4>
+				    <p class="card-text">Some example text. Some example text.</p>
+				    <a href="#" class="card-link btn btn-success">Explore</a>
+				    <a href="#" class="card-link">Featured Products</a>
+				  </div>
+				</div>
+			</div>
+			<!-- card 3 -->
+			<div class="col-sm-4">
+				<div class="card">
+				  <div class="card-body">
+				    <h4 class="card-title">Beauty Instruments</h4>
+				    <p class="card-text">Some example text. Some example text.</p>
+				    <a href="#" class="card-link btn btn-success">Explore</a>
+				    <a href="#" class="card-link">Featured Products</a>
+				  </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- contact information -->
+
+<div class="in-social">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				
+			</div>
+		</div>
+	</div>
+</div>
