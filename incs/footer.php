7d3e139c4129ab3f6b00e425a46055d0ebedd722
Ali Phularwan <victamoflove@gmail.com>
2020-03-20 11:08:08 +0500

    push

--- incs/footer.php
+++
@@ -1,5 +0,0 @@
-<footer style="background-color: #222; color: #ddd; padding: 5px 0px;">
-	<div>
-		<p class="text-center"> All Rights are Reserved by <a href="#"> Lexer International </a> 2020-2021 </p>
-	</div>
-</footer>
