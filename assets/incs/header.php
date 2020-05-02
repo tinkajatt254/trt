06e6d1dab3bd67f04a7f063512663f1df08240db
Ali Phularwan <victamoflove@gmail.com>
2020-05-02 16:24:15 +0500

    push new

--- assets/incs/header.php
+++ assets/incs/header.php
@@ -78,8 +78,8 @@
         <ul>
           <li class="active"><a href="#header">Home</a></li>
           <li><a href="index.php?page=about">About</a></li>
-          <li><a href="#services">Categories</a></li>
-          <li><a href="#contact">Contact</a></li>
+          <li><a href="index.php?page=category">Categories</a></li>
+          <li><a href="index.php?page=contact">Contact</a></li>
         </ul>
       </nav><!-- .nav-menu -->
     </div>
